41989c8260d2a3f72656c629ea677a39052580c9
thokomana75 <thokomana75@gmail.com>
2023-07-18 11:03:53 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,17 @@
 
 /**
  * print alphabet - print all alphabet lowercase
+ * Return: void
  */
 
 void print_alphabet(void)
+
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
 }
