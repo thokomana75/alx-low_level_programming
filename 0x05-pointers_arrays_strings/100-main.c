6b087e4ba2893092c8890722c3d9a0a28ec8f241
thokomana75 <thokomana75@gmail.com>
2023-07-24 14:28:08 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -11,6 +11,7 @@ int main(void)
 {
 
 	int nb;
+
 	nb = _atoi("98");
 	printf("%d\n", nb);
 	nb = _atoi("-402");
@@ -24,7 +25,7 @@ int main(void)
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
 	nb = _atoi("         +      +    -    -98 Battery Street;
-	San Francisco, CA 94111 - USA             ");
+			San Francisco, CA 94111 - USA             ");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	 printf("%d\n", nb);
