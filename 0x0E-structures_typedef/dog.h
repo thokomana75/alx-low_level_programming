61b7648b2f91f538ca7ea941fe5d0839d12cbdf9
thokomana75 <thokomana75@gmail.com>
2023-08-14 10:16:56 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define DOG_H
 
 /**
- * struct dog - A new type describing a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * *owner: The owner of the dog
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
